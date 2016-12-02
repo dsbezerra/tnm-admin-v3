c4b9dfebba2acee8bd68a4841467ef8b501ca36b
Diego Bezerra <dsouzaduarte@gmail.com>
2016-12-02 21:32:57 -0200

    Fix modalities

--- app/data/notice.js
+++ app/data/notice.js
@@ -4,6 +4,7 @@ export const ModalityOptions = [
   { text: 'Pregão Eletrônico', value: 1 },
   { text: 'Concorrência', value: 2 },
   { text: 'Convite', value: 3 },
-  { text: 'Leilão', value: 4 },
-  { text: 'Tomada de Preço', value: 5 }
+  { text: 'Concurso', value: 4 },
+  { text: 'Leilão', value: 5 },
+  { text: 'Tomada de Preço', value: 6 },
 ];
